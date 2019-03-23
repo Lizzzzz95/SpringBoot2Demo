70b1b669e1356b414e0aff939806a960f6ec985f
Lizzie Coady <lizziecoady@hotmail.co.uk>
2019-03-23 15:26:46 +0000

    Added delete and put methods

--- src/main/java/com/lizziecoady/SpringBoot2Demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/lizziecoady/SpringBoot2Demo/dao/FakePersonDataAccessService.java
@@ -20,4 +20,36 @@ public class FakePersonDataAccessService implements PersonDao {
     public List<Person> selectAllPeople() {
         return DB;
     }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+//        .stream accesses the database
+        return DB.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        Optional<Person> personMaybe = selectPersonById(id);
+        if (personMaybe.isEmpty()) {
+            return 0;
+        }
+        DB.remove(personMaybe.get());
+        return 1;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person personToUpdate) {
+        return selectPersonById(id)
+                .map(person -> {
+                    int indexOfPersonToUpdate = DB.indexOf(person);
+                    if (indexOfPersonToUpdate >= 0) {
+                        DB.set(indexOfPersonToUpdate, new Person(id, personToUpdate.getName()));
+                        return 1;
+                    }
+                    return 0;
+                })
+                .orElse(0);
+    }
 }
--- src/main/java/com/lizziecoady/SpringBoot2Demo/dao/PersonDao.java
+++ src/main/java/com/lizziecoady/SpringBoot2Demo/dao/PersonDao.java
@@ -17,4 +17,10 @@ public interface PersonDao {
 
     List<Person> selectAllPeople();
 
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id,Person person);
+
 }
